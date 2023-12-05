ec3c45631ded5a26db64162abf73ad6901e23bf3
ezraedl <50549215+ezraedl@users.noreply.github.com>
2023-12-05 23:00:12 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,83 @@
+enum RadioMessage {
+    message1 = 49434
+}
+let סיבוב = 0
+let כיוון = 0
+input.onButtonPressed(Button.A, function () {
+    סיבוב = 0
+    if (כיוון == -255) {
+        כיוון = 255
+    } else {
+        כיוון = -255
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    כיוון = 0
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . # . #
+        # . . . #
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    if (סיבוב == -1) {
+        סיבוב = 1
+    } else {
+        סיבוב = -1
+    }
+})
 basic.forever(function () {
-    for (let index = 0; index < 5; index++) {
-        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
-        SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
-        basic.pause(1000)
-        SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
-        SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
-        basic.pause(1000)
+    if (סיבוב != 0) {
+        if (סיבוב == 1) {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
+        } else {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
+        }
+        SuperBit.MotorRun(SuperBit.enMotors.M1, -1 * (סיבוב * 355))
+        SuperBit.MotorRun(SuperBit.enMotors.M3, סיבוב * 255)
+    } else {
+        if (כיוון == 255) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+        } else {
+            if (כיוון == -255) {
+                basic.showLeds(`
+                    . . # . .
+                    . # # # .
+                    # . # . #
+                    . . # . .
+                    . . # . .
+                    `)
+            } else {
+                basic.showLeds(`
+                    # # # # #
+                    # . . . #
+                    # . # . #
+                    # . . . #
+                    # # # # #
+                    `)
+            }
+        }
+        SuperBit.MotorRun(SuperBit.enMotors.M1, כיוון)
+        SuperBit.MotorRun(SuperBit.enMotors.M3, כיוון)
     }
 })
